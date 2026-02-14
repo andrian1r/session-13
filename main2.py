84dab9bb238b69527d4d43aeb6c2fc9e7462e6a2
andrian1r <andrianwijaya2701@gmail.com>
2026-02-14 15:57:26 +0700

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,26 @@
+# word_page = 0
+# pages = int(input("number of pages"))
+# word_per_page = int(input("numbers word per page:"))
+# total_word = pages * word_per_page
+# print(total_word)
+#
+#
+# def hitung_rata_rata(nilai):
+#     total = 0
+#
+#     for n in nilai:
+#         total += n
+#
+#
+#     rata2 = total / len(nilai)
+#     return rata2
+#
+# nilai_siswa = [80 , 90 , 75 , 95 ]
+# hasil = hitung_rata_rata(nilai_siswa)
+# print("Rata rata:", hasil)
+
+# def odd_or_even(number):
+#     if number % 2 == 0 :
+#         return "this is an even number. "
+#     else:
+#         return "this is an odd number. "
